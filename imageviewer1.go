4c6cc0fae6ee2c5da862d1979439d477e5d60567
goodhorce <goodhorce@163.com>
2013-11-01 21:32:07 +0800

    Commited
    First time

---
+++ imageviewer1.go
@@ -0,0 +1,154 @@
+// Copyright 2010 The Walk Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+        "log"
+        "fmt"
+        //"time"
+        "os"
+        "path/filepath"
+	//"path"
+	//"strings"
+)
+
+import (
+	"github.com/lxn/walk"
+	. "github.com/lxn/walk/declarative"
+	"github.com/lxn/win"
+)
+
+const (
+        imgpath = "d:\\image"
+        interval = 1
+)
+
+var imglist [100]string
+var pos     int
+var num     int
+
+
+func main() {
+        
+	mw := new(MyMainWindow)
+	//var openAction *walk.Action
+
+	if err := (MainWindow{
+		AssignTo: &mw.MainWindow,
+		Title:    "Walk Image Viewer Example",
+		Size:    Size{800, 600},
+		Layout:  HBox{},
+		Children: []Widget{
+			ImageView{
+				AssignTo: &(mw.imageView.iv),
+			},
+		},
+	}.Create()); err != nil {
+		log.Fatal(err)
+	} 
+
+        mw.SetFullscreen(true)
+
+        mw.initialize();
+
+        mw.Run()
+}
+
+type MyImageView struct {
+	iv *walk.ImageView 
+}
+
+type MyMainWindow struct {
+	*walk.MainWindow
+        imageView    MyImageView
+	prevFilePath string
+}
+
+func (mw *MyMainWindow) initialize() error {
+    pos, num = 0, 1
+    getfiles()
+    pos = 1
+    mw.imageView.openImage()
+    return nil
+}
+
+func getfiles() error {
+	if err := filepath.Walk(imgpath, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			if info == nil {
+				return filepath.SkipDir
+			}
+		}
+
+		if info.IsDir() || num>100 {
+		} else {
+                    imglist[num] = path
+                    num += 1
+                }
+
+		return nil
+	}); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+/*
+func (mw *MyMainWindow) openAction_Triggered() {
+	if err := mw.displayFiles(imgpath); err != nil {
+		log.Print(err)
+	}
+}
+*/
+
+func (iv *MyImageView) openImage() error {
+
+	img, err := walk.NewImageFromFile(imglist[pos])
+	if err != nil {
+		return err
+	}
+
+	var succeeded bool
+	defer func() {
+		if !succeeded {
+			img.Dispose()
+		}
+	}()
+
+
+	if err := iv.iv.SetImage(img); err != nil {
+		return err
+	}
+
+        iv.iv.CustomWidget.Invalidate()
+
+	succeeded = true
+
+	return nil
+}
+
+func (iv *MyImageView) WndProc(hwnd win.HWND, msg uint32, wParam, lParam uintptr) uintptr {
+    fmt.Println("aaa")
+    switch msg {
+        case win.WM_LBUTTONDOWN:
+            pos += 1
+            iv.openImage()
+        case win.WM_KEYDOWN:
+            switch wParam {
+                case win.VK_LEFT: fallthrough
+                case win.VK_UP:
+                    pos -= 1
+                    iv.openImage()
+                case win.VK_RIGHT: fallthrough
+                case win.VK_DOWN:
+                    pos += 1
+                    iv.openImage()
+            }
+    }
+    
+    return iv.iv.WidgetBase.WndProc(hwnd, msg, wParam, lParam)
+}
+
---
+++ readme.txt
@@ -0,0 +1,6 @@
+This is a go program I write to show a list of photos. 
+But I run into some issues I have not found answers. 
+1. I use walk, but not sure why ImageView can't handle messages. 
+2. I want to show photos automatically, I call time.Sleep but GUI can't be updated in time. 
+
+I am appreciated for any help. 
